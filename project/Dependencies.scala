33f5d603379bad2326284926da5221f368320c29
Victor Osipov <osipovvictor1994@gmail.com>
2024-07-16 10:36:52 +0100

    DDCE-5783 - Remove scalastyle, update deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,10 @@ import sbt.*
 
 object Dependencies {
 
-  private val gatlingVersion = "3.6.1"
-
   private val test: Seq[ModuleID] = Seq(
-    "io.gatling"            % "gatling-test-framework"    % gatlingVersion,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.6.0"
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.0.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = test
+
 }
