cd97b5e546f469005704eb3d859925ca5035b494
Gaston Racui <gastonracui@gmail.com>
2025-05-16 06:47:53 +0100

    DDCE-6604 Standardise repository.yaml

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   private val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "performance-test-runner" % "6.0.0"
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = test
